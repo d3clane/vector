cf202c4bdadf47be4f3ecbdfc3f1a8ed76c49051
Arman <ovakimian.al@phystech.edu>
2024-10-10 20:54:21 +0300

    fixed common vector

--- include/CommonVectorFuncs.hpp
+++ include/CommonVectorFuncs.hpp
@@ -9,35 +9,6 @@
 namespace MyStd
 {
 
-char* allocateMemoryInBytes(size_t sizeInBytes)
-{
-    char* data = nullptr;
-
-    try
-    {
-        data = new char[sizeInBytes];
-    }
-    catch (const std::bad_alloc& e)
-    {
-        throw EXCEPTION_WITH_REASON_CREATE_NEXT_EXCEPTION(
-            StdErrors::MemAllocErr,
-            "Failed to allocate memory for vector",
-            {}
-        );
-    }
-
-    return data;
-}
-
-template<typename T>
-void dtorElements(T* data, size_t from, size_t to) noexcept
-{
-    for (size_t i = from; i < to; ++i)
-    {
-        data[i].~T();
-    }
-}
-
 inline size_t getCapacityAfterGrowth(size_t capacity) noexcept
 {
     static const size_t growthFactor = 2;
